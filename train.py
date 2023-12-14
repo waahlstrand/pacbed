9efde5812e0da53918ce3759d34c574a3d41e0db
Victor Wåhlstrand Skärström <victor.wahlstrand.skarstrom@chalmers.se>
2023-12-14 20:56:55 +0100

    New naming

--- train.py
+++ train.py
@@ -81,7 +81,12 @@ def main():
 
 def fit(args: argparse.Namespace):
 
-    name = "_".join([args.name, args.target, str(args.convergence_angle_index), str(args.energy_index)])
+    name = "_".join([
+        args.name, 
+        args.target.replace(" ", "_"),
+        str(args.crop), 
+        str(args.convergence_angle_index), 
+        str(args.energy_index)])
 
     # Set the random seed
     L.seed_everything(args.seed)
